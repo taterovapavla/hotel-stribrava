cca99cca87f6157640c7142b61e73542ec11ea5b
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-11-18 14:22:01 +0100

    get orders on admin page

--- src/pages/AdminPage/index.jsx
+++ src/pages/AdminPage/index.jsx
@@ -1,9 +1,20 @@
 import './style.css';
+import { useState, useEffect } from 'react';
 
 export const AdminPage = () => {
+  const [orders, setOrders] = useState();
+  useEffect(() => {
+    const getOrders = async () => {
+      const response = await fetch('http://localhost:4000/api/orders');
+      const data = await response.json();
+      setOrders(data.result);
+    }
+    getOrders();
+  }, [])
   return (
     <div className="container">
       Admin
+      {orders ? JSON.stringify(orders) : null}
     </div>
   );
 };
