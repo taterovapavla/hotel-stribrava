6e210a0c6f0cec742a56572e22b6387affde0a6e
Pavla Taterová <taterova.pavla@gmail.com>
2023-11-18 11:28:16 +0100

    adding components

---
+++ src/pages/components/Header/index.jsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return (
+    <div class="banner">
+    <div class="banner__stripe">
+      <div class="container">
+        <h1>Template</h1>
+        <p class="lead">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, sequi.
+        </p>
+      </div>
+    </div>
+  </div>
+  )
+}
---
+++ src/pages/components/RoomList/index.jsx
@@ -0,0 +1,19 @@
+export const RoomList = () => {
+  return (
+    <section class="dark">
+    <div class="container">
+      <h2>Heading</h2>
+      <p>
+        Quas odio quidem, enim nihil unde quia temporibus vitae in ab.
+      </p>
+      <div class="cards-row">
+        <div class="card">
+          <img class="card__image" src="img/image1.svg" />
+          <div class="card__title">Card 1</div>
+          <div class="card__body">Sunt natus</div>
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
